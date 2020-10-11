04dedd6a7c75041f6925958d8cd9a112fc991b11
Edinson Cotrina Ramos <e.cotrinar@pucp.edu.pe>
2020-10-11 01:19:20 -0500

    Empezando mejora

---
+++ src/src/grasp/LeerCsv.java
@@ -0,0 +1,47 @@
+package grasp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class LeerCsv {
+    public ArrayList<LugarCobro> leer(){
+        ArrayList<LugarCobro> datos = new ArrayList<LugarCobro>();
+        Path filepath = Paths.get("C:\\Users\\USUARIO\\Downloads\\as.csv");
+        try{
+            BufferedReader br = Files.newBufferedReader(filepath);
+            String linea,cab;
+            cab=br.readLine();
+            while((linea=br.readLine())!=null){
+                String[] datosDeLinea =linea.split(";");
+                LugarCobro lugar= new LugarCobro(datosDeLinea);
+                datos.add(lugar);
+            }
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return datos;
+    }
+    public ArrayList<Beneficiario> leerBene(){
+        ArrayList<Beneficiario> datos = new ArrayList<Beneficiario>();
+        Path filepath = Paths.get("C:\\Users\\USUARIO\\Downloads\\bs.csv");
+        try{
+            BufferedReader br = Files.newBufferedReader(filepath);
+            String linea,cab;
+            cab=br.readLine();
+            while((linea=br.readLine())!=null){
+                String[] datosDeLinea =linea.split(";");
+                Beneficiario lugar= new Beneficiario(datosDeLinea);
+                datos.add(lugar);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return datos;
+    }
+    
+}
